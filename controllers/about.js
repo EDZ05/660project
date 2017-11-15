0145b1f8e72a09fe0785a32e7d1cbc1c5e6ca06a
Edward Dcruz <edward.dcruz@yale.edu>
2017-11-15 22:24:18 +0000

    Updated About page to render images

--- controllers/about.js
+++ controllers/about.js
@@ -1,11 +1,14 @@
 
 // Create a function which is a "controller", it
 // handles a request, writing the response.
+
+
 function about(request, response) {
-    response.render('about', { title: 'Hello Yale SOM hackers part 2' });
+    var contextData = {};
+    response.render('about.html', contextData);
 }
 
 module.exports = {
-    about,
+    about: about,
 };
 
