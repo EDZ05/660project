67f99b06db3f5efa122f5336710b5b6b1118174c
andrew kim <kimand@oregonstate.edu>
2017-11-16 03:40:22 +0000

    not sure..

--- controllers/index.js
+++ controllers/index.js
@@ -3,16 +3,10 @@ const eventModels = require('../models/events.js');
 // Create a function which is a "controller", it
 // handles a request, writing the response.
 function index(request, response) {
-<<<<<<< HEAD
     const contextData = {
         title: 'Hello Yale SOM hackers',
         events: eventModels.all,
     }
-=======
-    const contextData = { 
-        title: 'Hello Yale SOM hackers',
-        events: listofEvents.all
->>>>>>> d3ce845b8c2f30fcd26e20c975ccc80764782d6e
     response.render('index', contextData);
 }
 
