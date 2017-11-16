0ca86f55a67fb1ce1b74d9bef52ab6bc6c2d3e24
andrew kim <kimand@oregonstate.edu>
2017-11-16 03:27:41 +0000

    hmm

--- controllers/index.js
+++ controllers/index.js
@@ -1,21 +1,15 @@
-const listofEvents = require('../models/events.js');
+const eventModels = require('../models/events.js');
 
 // Create a function which is a "controller", it
 // handles a request, writing the response.
 function index(request, response) {
-    response.render('index', { title: 'Hello Yale SOM hackers' });
-}
-
-function listEvents(request, response) {
-    const query = request.query.q;
     const contextData = {
-        title: 'List of events',
-        eventMatchingQuery: listofEvents,
-    };
-    response.render('events', contextData);
+        title: 'Hello Yale SOM hackers',
+        events: eventModels.all,
+    }
+    response.render('index', contextData);
 }
 
 module.exports = {
     index,
-    listEvents,
 };
