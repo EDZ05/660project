cf17736178391c0e17bf35959e5db4ab6f7edf5a
shiverz <shiv.sinha@yale.edu>
2017-12-07 07:11:56 +0000

    remove pg from server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
-var pg = require('pg');
+//var pg = require('pg');
 
 var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
